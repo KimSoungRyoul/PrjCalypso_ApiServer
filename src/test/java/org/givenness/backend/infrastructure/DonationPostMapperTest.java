96e7faace2a6135b328e792902165a3b5db41763
KimSoungRyoul <KimSoungRyoul@gmail.com>
2018-07-06 17:21:22 +0900

    MemberMapper작성2 2018-07-06(2)

--- src/test/java/org/givenness/backend/infrastructure/DonationPostMapperTest.java
+++ src/test/java/org/givenness/backend/infrastructure/DonationPostMapperTest.java
@@ -1,9 +1,26 @@
 package org.givenness.backend.infrastructure;
 
 
+import org.givenness.backend.infrastructure.mapper.DonationPostMapper;
+import org.givenness.backend.model.post.DonationPost;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("dev")
 public class DonationPostMapperTest extends DefaultAbstractTest {
+
+  @Autowired
+  private DonationPostMapper donationPostMapper;
+
+
+  @Test
+  public void selectOnePost() {
+
+    DonationPost donationPost = donationPostMapper.selectOnePost(1L);
+
+
+  }
+
 
 }
